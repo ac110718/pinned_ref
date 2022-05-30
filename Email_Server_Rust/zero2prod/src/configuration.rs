be064f2aecbb3e179685f7f22648a8bd0916360c
ac110718 <allen.h.chan@gmail.com>
2022-05-30 09:30:06 -0400

    integration test configurations

--- Email_Server_Rust/zero2prod/src/configuration.rs
+++ Email_Server_Rust/zero2prod/src/configuration.rs
@@ -1,1 +1,41 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
 
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // initialize configuration reader
+    let mut settings = config::Config::default();
+
+    // add configuration values from file named `configuration` (yaml, json etc)
+    settings.merge(config::File::with_name("configuration"))?;
+
+    // convert values into Settings struct type per above
+    settings.try_into()
+}
