7a3acc892dbb84a0e1b8fa09d49d56a58373e901
ac110718 <allen.h.chan@gmail.com>
2022-05-30 11:22:09 -0400

    telemetry refactoring

--- Email_Server_Rust/zero2prod/src/telemetry.rs
+++ Email_Server_Rust/zero2prod/src/telemetry.rs
@@ -1,11 +1,7 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
 
 pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
     let env_filter =
@@ -20,17 +16,4 @@ pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sen
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
-}
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
-    init_subscriber(subscriber);
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
 }
