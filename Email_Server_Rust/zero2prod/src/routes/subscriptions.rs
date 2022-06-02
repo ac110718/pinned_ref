dc8ba324c935b465629532e233f1e3b68d64593b
ac110718 <allen.h.chan@gmail.com>
2022-06-02 09:25:09 -0400

    error::source

--- Email_Server_Rust/zero2prod/src/routes/subscriptions.rs
+++ Email_Server_Rust/zero2prod/src/routes/subscriptions.rs
@@ -129,7 +129,6 @@ pub async fn store_token(
     Ok(())
 }
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl ResponseError for StoreTokenError {}
@@ -141,6 +140,33 @@ impl std::fmt::Display for StoreTokenError {
             "A database error was encountered while trying to store a subscription token."
         )
     }
+}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+// dyn Error is trait object, which allows for runtime polymorphism. Nothing is known except it implements Error trait
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+// iterates over entire chain of errors that led to original error
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
 }
 
 #[tracing::instrument(
