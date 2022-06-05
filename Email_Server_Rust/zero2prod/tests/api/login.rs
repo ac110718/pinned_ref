e6f9f0b8706cb6313162f2ebf9ab444743f4a7b2
ac110718 <allen.h.chan@gmail.com>
2022-06-05 15:19:40 -0400

    cookies rather than hmac

--- Email_Server_Rust/zero2prod/tests/api/login.rs
+++ Email_Server_Rust/zero2prod/tests/api/login.rs
@@ -1,10 +1,7 @@
-use crate::helpers::assert_is_redirect_to;
-use crate::helpers::spawn_app;
-use reqwest::header::HeaderValue;
-use std::collections::HashSet;
+use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
-async fn an_error_flash_message_is_set_on_failer() {
+async fn an_error_flash_message_is_set_on_failure() {
     let app = spawn_app().await;
 
     let login_body = serde_json::json!({
@@ -13,8 +10,8 @@ async fn an_error_flash_message_is_set_on_failer() {
     });
     let response = app.post_login(&login_body).await;
 
-    assert_eq!(response.status().as_u16(), 303);
     assert_is_redirect_to(&response, "/login");
-    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
-    assert_eq!(flash_cookie.value(), "Authentication failed");
+
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains("<p><i>Authentication failed</i></p>"));
 }
