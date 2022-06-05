cc26a2b3a9565dfeb949f67e5a2492a09e70a838
ac110718 <allen.h.chan@gmail.com>
2022-06-05 17:01:53 -0400

    cookie default delete

--- Email_Server_Rust/zero2prod/src/routes/login/get.rs
+++ Email_Server_Rust/zero2prod/src/routes/login/get.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::Cookie;
 use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
@@ -7,7 +8,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
             format!("<p><i>{}</i></p>", cookie.value())
         }
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
@@ -37,5 +38,9 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
               </form>
           </body> 
           </html>"#,
-        ))
+        ));
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .unwrap();
+    response
 }
